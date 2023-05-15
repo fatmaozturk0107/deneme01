33bba2c836068aaad9b812ad3343839d5931a98b
Fatma Öztürk <candagasan2016@gmail.com>
2023-05-15 11:41:07 +0300

    ilk değişiklik

--- src/main/java/gitHubdeneme1/Deneme.java
+++ src/main/java/gitHubdeneme1/Deneme.java
@@ -3,5 +3,6 @@ package gitHubdeneme1;
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("merhaba dünya bu işi başaracağım");
     }
 }
