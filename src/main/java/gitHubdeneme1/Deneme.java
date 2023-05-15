2a01e091930e34c814329590dc2acf2f179243f7
Fatma Öztürk <candagasan2016@gmail.com>
2023-05-15 13:02:15 +0300

    son değişiklik

--- src/main/java/gitHubdeneme1/Deneme.java
+++ src/main/java/gitHubdeneme1/Deneme.java
@@ -4,5 +4,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("merhaba dünya bu işi başaracağım");
+        System.out.println("selam nasılsın");
     }
 }
